fabd183ee8de9126cc8c9ac51362714babd45710
Aleksandr Kruglov <alkruglov777@gmail.com>
2018-08-19 01:59:33 +0300

    Add fund type constants

---
+++ src/main/java/com/feature/item/model/ItemTypeConst.java
@@ -0,0 +1,8 @@
+package com.feature.item.model;
+
+public interface ItemTypeConst {
+    String INCOME = "INCOME";
+    String EXPENSE = "EXPENSE";
+    String INCOME_FUND = "INCOME_FUND";
+    String EXPENSE_FUND = "EXPENSE_FUND";
+}
