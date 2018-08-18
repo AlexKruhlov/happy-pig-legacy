bf185a53a89ebe2e620a5aa2289eb6dbbeefac00
Aleksandr Kruglov <alkruglov777@gmail.com>
2018-08-18 23:35:31 +0300

    Add readonly and transaction properties

--- src/main/java/com/feature/fund/repository/FundRepositoryImpl.java
+++ src/main/java/com/feature/fund/repository/FundRepositoryImpl.java
@@ -3,8 +3,10 @@ package com.feature.fund.repository;
 import com.api.fund.repository.FundRepository;
 import com.feature.fund.model.Fund;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.ReadOnlyProperty;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -18,16 +20,19 @@ public class FundRepositoryImpl implements FundRepository {
     }
 
     @Override
+    @ReadOnlyProperty
     public List<Fund> findAll() {
         return (List<Fund>) fundDefaultRepository.findAll();
     }
 
     @Override
+    @ReadOnlyProperty
     public Fund findById(String id) {
         return fundDefaultRepository.findById(id).orElse(null);
     }
 
     @Override
+    @Transactional
     public Fund save(Fund fund) {
         return fundDefaultRepository.save(fund);
     }
--- src/test/java/com/feature/fund/repository/FundRepositoryTest.java
+++ src/test/java/com/feature/fund/repository/FundRepositoryTest.java
@@ -2,7 +2,6 @@ package com.feature.fund.repository;
 
 import com.App;
 import com.api.fund.repository.FundRepository;
-import com.feature.fund.dto.FundDto;
 import com.feature.fund.model.Fund;
 import com.feature.item.model.Item;
 import org.junit.Test;
@@ -10,27 +9,22 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Profile;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @ComponentScan(basePackageClasses = App.class)
 public class FundRepositoryTest {
     public static final String FUND_ID = "GROCERY_FUND";
+    public static final String FUND_TYPE = "INCOME";
     public static final String WRONG_FUND_ID = "WRONG_FUND_ID";
     public static final int ALL_FUND_COUNT = 3;
 
@@ -58,32 +52,23 @@ public class FundRepositoryTest {
 
     @Test
     public void shouldSaveFund() {
-//        Fund fund = Fund.builder().name("Family Car").items(singletonList(
-//           Item.builder().date(LocalDateTime.now()).amount(BigInteger.valueOf(1000)).type("INCOME").build()
-//        )).build();
-//        Fund storedFund = fundRepository.save(fund);
-//        assertNotNull(storedFund);
-//        List<Fund> fund1 = fundRepository.findAll();
-//        assertEquals(storedFund, fundRepository.findAll());
-//
-//        System.out.println(fundRepository.findAll().stream().peek(System.out::println));
+        Fund fund = Fund.builder().name("Family Car").build();
+        Fund storedFund = fundRepository.save(fund);
+        assertNotNull(storedFund.getId());
     }
 
     @Test
     public void shouldUpdateFund(){
-//        Fund fund = Fund.builder().name("Family Car").build();
-//        List<Item> items = new ArrayList<>();
-//        items.add(Item.builder().type("INCOME").date(LocalDateTime.now()).amount(BigInteger.TEN).build());
-//        fund.setItems(items);
-//
-//        Fund storedFund = fundRepository.save(fund);
-//        assertNotNull(storedFund);
-//
-//        storedFund.setName("Private Car");
-//        Fund twiceStoredFund = fundRepository.save(storedFund);
-//        System.out.println(twiceStoredFund.getItems().get(0).getFundId());
-//        assertNotNull(twiceStoredFund.getId());
-//        System.out.println(fundRepository.findById(storedFund.getId()).getItems().get(0).getId());
-//        assertEquals(twiceStoredFund, fundRepository.findById(storedFund.getId()));
+        Fund fund = fundRepository.findById(FUND_ID);
+        fund.getItems().add(
+                Item.builder()
+                        .amount(BigInteger.TEN)
+                        .date(LocalDateTime.now())
+                        .type(FUND_TYPE)
+                        .fundId(FUND_ID).build()
+        );
+        fundRepository.save(fund);
+        Fund updatedFund = fundRepository.findById(FUND_ID);
+        assertEquals(fund, updatedFund);
     }
 }
