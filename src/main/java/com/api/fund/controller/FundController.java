104f9b234d57a27066837ebc1092ca32f8338b8d
Aleksandr Kruglov <alkruglov777@gmail.com>
2018-09-08 22:23:24 +0300

    HP-00027: Add java docs to api

--- src/main/java/com/api/fund/controller/FundController.java
+++ src/main/java/com/api/fund/controller/FundController.java
@@ -5,15 +5,52 @@ import com.feature.fund.dto.FundDtoWithItems;
 
 import java.util.List;
 
+/**
+ * Provides the endpoints to work on funds
+ */
 public interface FundController {
 
+    /**
+     * Finds the fund with the particular id
+     *
+     * @param id fund id
+     * @return fund with the same id
+     * @see FundDto
+     */
     FundDto findById(String id);
 
+    /**
+     * Finds all funds
+     *
+     * @return list of all funds
+     * @see FundDto
+     */
     List<FundDto> findAll();
 
+    /**
+     * Updates existed fund if it is existed, otherwise saves the fund
+     *
+     * @param fundDtoWithItems fund with items object
+     * @return all funds with items
+     * @see FundDtoWithItems
+     */
     FundDtoWithItems update(FundDtoWithItems fundDtoWithItems);
 
+    /**
+     * Creates new fund
+     *
+     * @param fundDto fund object to crate
+     * @return all funds
+     * @see FundDto
+     */
     List<FundDto> saveAndFindAll(FundDto fundDto);
 
+    /**
+     * Deletes existed fund with particular id
+     *
+     * @param id fund id to delete
+     * @return all funds
+     * @see FundDto
+     */
     List<FundDto> deleteByIdAndFindAll(String id);
 }
--- src/main/java/com/api/fund/service/FundService.java
+++ src/main/java/com/api/fund/service/FundService.java
@@ -5,16 +5,53 @@ import com.feature.fund.dto.FundDtoWithItems;
 
 import java.util.List;
 
+/**
+ * Provides possibility of working on funds business logic
+ */
 public interface FundService {
 
+    /**
+     * Finds fund model object with particular id and transform it to dto
+     *
+     * @param id fund id
+     * @return fund with items
+     * @see FundDtoWithItems
+     */
     FundDtoWithItems findById(String id);
 
+    /**
+     * Finds all funds and transforms them into dtos
+     *
+     * @return fund dtos
+     * @see FundDto
+     */
     List<FundDto> findAll();
 
+    /**
+     * Updates (saves) the fund that has been transformed from dto
+     * and returns it with transformation into dto
+     *
+     * @param fundDtoWithItems fund dto
+     * @return fund dto
+     * @see FundDtoWithItems
+     */
     FundDtoWithItems update(FundDtoWithItems fundDtoWithItems);
 
+    /**
+     * Updates (saves) the fund that has been transformed from dto
+     * and ruturns it with transformation into dto
+     *
+     * @param fundDto fund dto
+     * @return fund dto
+     * @see FundDto
+     */
     FundDto save(FundDto fundDto);
 
+    /**
+     * Deletes fund by its id
+     *
+     * @param id fund id
+     */
     List<FundDto> saveAndFindAll(FundDto fundDto);
 
     List<FundDto> deleteByIdAndFindAll(String id);
